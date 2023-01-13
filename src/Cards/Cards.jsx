a3da8bcab5ac3020b1f18384d9097cac35ed95b6
willian <williandeitosi@gmail.com>
2023-01-12 22:39:09 -0300

    creating the tracker

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -8,25 +8,51 @@ export default function Cards() {
   return (
     <div id="app">
       <div id="player-1" className="player">
-        <img src={hans} alt="" />
-        <div>
-          <h1>Acorda Devinho</h1>
-          <p>Banda Rocketseat</p>
+        <div className="wrapper">
+          <img
+            className="tumb"
+            src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            alt="img abstract"
+          />
+
+          <div className="info">
+            <h1>Acorda Devinho</h1>
+            <p>Banda Rocketseat</p>
+          </div>
+
+          <div className="controls">
+            <img src={back} alt="" />
+            <img src={play} alt="" />
+            <img src={next} alt="" />
+          </div>
+
+          <div className="track-time">
+            <div className="tracker"></div>
+
+            <div className="timer">
+              <div className="total-time">03:20</div>
+              <div className="last-time">00:12</div>
+            </div>
+          </div>
         </div>
-        <div>
-          <img src={back} alt="" />
-          <img src={play} alt="" />
-          <img src={next} alt="" />
-        </div>
-        <div></div>
       </div>
 
       <div id="player-2" className="player">
-        teste 2
+        {/* <div className="wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80"
+            alt=""
+          />
+        </div> */}
       </div>
 
       <div id="player-3" className="player">
-        teste3
+        {/* <div className="wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80"
+            alt=""
+          />
+        </div> */}
       </div>
     </div>
   );
