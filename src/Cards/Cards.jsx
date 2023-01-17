9ad5128b44f54b53c0b2be5bebdf5ac7d92d18e7
willian <williandeitosi@gmail.com>
2023-01-16 22:23:55 -0300

    completed the HTML and CSS

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -38,21 +38,55 @@ export default function Cards() {
       </div>
 
       <div id="player-2" className="player">
-        {/* <div className="wrapper">
-          <img
-            src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80"
-            alt=""
-          />
-        </div> */}
+        <div className="wrapper">
+          <div className="info-wrapper">
+            <img
+              className="tumb"
+              src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="img abstract"
+            />
+            <div className="info">
+              <h1>Acorda Devinho</h1>
+              <p>Banda Rocketseat</p>
+            </div>
+          </div>
+
+          <div className="controls-mini">
+            <img src={back} alt="" />
+            <img src={play} alt="" />
+            <img src={next} alt="" />
+          </div>
+
+          <div className="track-time">
+            <div className="tracker"></div>
+
+            <div className="timer">
+              <div className="total-time">03:20</div>
+              <div className="last-time">00:12</div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div id="player-3" className="player">
-        {/* <div className="wrapper">
-          <img
-            src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80"
-            alt=""
-          />
-        </div> */}
+        <div className="wrapper">
+          <div className="info-wrapper">
+            <img
+              className="tumb"
+              src="https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="img abstract"
+            />
+            <div className="info">
+              <h1>Acorda Devinho</h1>
+              <p>Banda Rocketseat</p>
+            </div>
+          </div>
+          <div className="controls-mini">
+            <img src={back} alt="" />
+            <img src={play} alt="" />
+            <img src={next} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
